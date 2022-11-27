77d3abae8b4e00b3be43a3ef995fa82c510c0592
elvina22222 <114909756+elvina22222@users.noreply.github.com>
2022-11-27 16:42:41 +0400

    add html

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var input = document.querySelector('input');
-var exlude = [8, 46, 37 , 39]
+var exclude = [8, 46, 37 , 39]
 input.addEventListener('keydown',function(e){
     var code = e.keyCode 
-    if (!(code >= 48 && code <= 57 ) && !!!exlude.find( k => k === +code)){
+    if (!(code >= 48 && code <= 57 ) && !!!exclude.find( k => k === +code)){
         e.preventDefault()
         return false;
     }
@@ -10,4 +10,19 @@ input.addEventListener('keydown',function(e){
     if ((code >= 48 && code <= 57))
         if (len % 4 === 0 && len <50 && code!==8)
             e.target.value += " "
+})
+
+const currency = document.querySelector('.cur');
+const one = document.querySelector('.first');
+const two = document.querySelector('.second');
+const rub = document.querySelector('.rub');
+const usd = document.querySelector('.usd');
+let a=0;
+currency.addEventListener("click", function() {
+    if (currency == rub ){
+            c=one.value*1.68;
+                 console.log(conamount.value +' EUR = ' + c +' AZN');
+          }
+    }
+    
 })
